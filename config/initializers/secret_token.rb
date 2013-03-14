a02f4be02b464f583de8dbc0b896a96d82bf2a36
Shawn Lee-Kwong <splitnova@gmail.com>
2013-03-13 20:09:19 -0400

    new rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LcjewelleryCollaboration::Application.config.secret_token = '672eceff9000bea525da0bf287729fe41d901670dd4123dd9cd9956f08df823d73a71bca121057041784664be6cd5ce01d94f8bc1d64e47371a0e8084a4261f5'
